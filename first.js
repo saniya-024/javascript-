b3f574692e5d5b985e5a0033c481f49b11c01a91
saniya-024 <saniyayadav0208@gmail.com>
2026-04-16 10:43:45 +0530

    index.html

--- first.js
+++ first.js
@@ -1,10 +1,21 @@
-let age = 24;
-let x = BigInt("123") 
-const student = {
-    fullName:"Aman",
-    age:20,
-    cgpa:8,
-    isPass:true,
-};
-console.log(student["age"]);
-console.log(student.cgpa);
+// let age = 24;
+// let x = BigInt("123") 
+// const student = {
+//     fullName:"Aman",
+//     age:20,
+//     cgpa:8,
+//     isPass:true,
+// };
+// console.log(student["age"]);
+// console.log(student.cgpa);
+
+function addsum() {
+    let a = document.getElementById("number1").value;
+    let b = document.getElementById("number2").value;
+
+    let sum = Number(a) + Number(b);
+
+    console.log("Total number", sum);
+
+    document.getElementById("result").innerText = sum;
+}
