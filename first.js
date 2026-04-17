f834c9b80d4e212b9110e09cfbe63cb0e0e6c37e
saniya-024 <saniyayadav0208@gmail.com>
2026-04-17 10:19:41 +0530

    arithmetic operator start

--- first.js
+++ first.js
@@ -1,21 +1,3 @@
-// let age = 24;
-// let x = BigInt("123") 
-// const student = {
-//     fullName:"Aman",
-//     age:20,
-//     cgpa:8,
-//     isPass:true,
-// };
-// console.log(student["age"]);
-// console.log(student.cgpa);
-
-function addsum() {
-    let a = document.getElementById("number1").value;
-    let b = document.getElementById("number2").value;
-
-    let sum = Number(a) + Number(b);
-
-    console.log("Total number", sum);
-
-    document.getElementById("result").innerText = sum;
-}
+let a=3;
+let b=5;
+console.log("a+b=",a=b);
