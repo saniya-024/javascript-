22dfa519dd7d1f095d0f998224a8f16cffd80b97
saniya-024 <saniyayadav0208@gmail.com>
2026-04-17 12:38:48 +0530

    variables question completed

--- first.js
+++ first.js
@@ -1,10 +1,25 @@
-//questionssss//
-let name="Sana";
-console.log(name);
+//questionssss  of variables
+// // let name="Sana";
+// // console.log(name);
 
 
-let age=25;
-console.log(age);
+// let age=25;
+// console.log(age);
 
-const country="India";
-console.log(country);
+// const country="India";
+// console.log(country);
+// //names
+
+// let name = "Saniya";
+
+// console.log("Before:", name);
+
+// name = "Riya";
+
+// console.log("After:", name);
+
+
+const name="Sana";
+console.log("Before:",name);
+ const name= "riya";
+ console.log("after;",name);
