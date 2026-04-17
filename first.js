61e7beb80f4139fdf0e3701710e23aee249939a5
saniya-024 <saniyayadav0208@gmail.com>
2026-04-17 11:43:18 +0530

    ternery operator

--- first.js
+++ first.js
@@ -1,14 +1,4 @@
-let a=3;
-let b=5;
-console.log("a+b=",a=b);
-//unary operator
-a=a+1;
-console.log("a=",a);
-//assignment operators
-a+=5;
-console.log("a=",a);
-//conditional statement
-let age=25;
-if(age>20){
-    console.log("you can vote");
-}
+//ternery operator
+let age = 25;
+let result = age >= 18 ? "adult" : "not adult";
+console.log (result);
